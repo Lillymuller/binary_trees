c86254bdde34b2b5d57a26b42ea9b683890cf9a6
Lillymuller <eleltaalemu@gmail.com>
2024-02-27 04:08:39 -0800

    Creat new node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -32,10 +32,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	parent->right = right_child;
 
 	return (right_child);
-
-	if (right_child == NULL)
-		return (NULL);
-
-	if (parent == NULL)
-		return (NULL);
 }
