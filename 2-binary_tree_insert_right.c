2d6e55f173edec4ecbbe4f9e6fba50b14203a8f6
Lillymuller <eleltaalemu@gmail.com>
2024-02-27 03:32:18 -0800

    creat right-node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,13 +15,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_child;
 
+	if (parent == NULL)
+		return (NULL);
+
 	right_child = binary_tree_node(parent, value);
+
+	if (right_child == NULL)
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
 		right_child->right = parent->right;
 		parent->right->parent = right_child;
 	}
+
 	parent->right = right_child;
 
 	return (right_child);
