a2fc7eeead160aaea140a38ab58267437bb43601
Lillymuller <eleltaalemu@gmail.com>
2024-02-27 03:44:13 -0800

    creat a new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,8 +4,7 @@
  * binary_tree_node - Creates a binary tree new node.
  * @parent: Pointer to the parent of new node.
  * @value: Value of new node.
- *
- * Return: Pointer to new node (Success) or Null (Faliure).
+ * Return: Pointer to new node success or Null faliure.
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
